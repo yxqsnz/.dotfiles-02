6dc776c4d3606ab51e1e465f57bdc7f9916b4f52
yxqsnz <yxqsnz@gmail.com>
2022-11-21 19:14:10 -0300

    feat: waybar & sway basic config

---
+++ config/shell/fish/conf.d/path.fish
@@ -0,0 +1,6 @@
+set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH $HOME/.yarn/bin  $PATH
+set -gx PATH $HOME/.cargo/bin $PATH
+set -gx PATH /sbin 	      $PATH
+
+source ~/.asdf/asdf.fish
--- dot.sh
+++ dot.sh
@@ -3,60 +3,63 @@
 COLOR_RED='\033[0;31m'
 COLOR_GREEN='\033[0;32m'
 COLOR_CYAN='\033[0;36m'
-NO_COLOR='\033[0m' 
+NO_COLOR='\033[0m'
 
 declare -A dots=(
-  ["wm/sway"]="${HOME}/.config/sway"
-  ["term/kitty"]="${HOME}/.config/kitty"
-  ["shell/fish"]="${HOME}/.config/fish"
-  ["dev/git"]="${HOME}/.config/git"
-) 
+	["wm/sway"]="${HOME}/.config/sway"
+	["wm/waybar"]="${HOME}/.config/waybar"
+	["term/kitty"]="${HOME}/.config/kitty"
+	["shell/fish"]="${HOME}/.config/fish"
+	["dev/git"]="${HOME}/.config/git"
+	["compilers/cargo.toml"]="${HOME}/.cargo/config.toml"
+)
 
 _exists() {
 	[[ -f $1 ]] || [[ -d $1 ]] || readlink "$1" >/dev/null
 }
 
 _link() {
-  for dotfile in "${!dots[@]}"; do
-    out="${dots[$dotfile]}"
-    echo -en " ${COLOR_CYAN}Install${NO_COLOR} ${dotfile}"
+	for dotfile in "${!dots[@]}"; do
+		out="${dots[$dotfile]}"
+		echo -en " ${COLOR_CYAN}Install${NO_COLOR} ${dotfile}"
 
-    if ! _exists "$out"; then
+		if ! _exists "$out"; then
 			ln -s "$(realpath "config/${dotfile}")" "$out"
 			echo -e " [ ${COLOR_GREEN}OK${NO_COLOR} ]"
 		else
 			echo -e " [ ${COLOR_RED}IGNORED${NO_COLOR} ]"
 		fi
-  done
+	done
 }
 
 _unlink() {
-  for dotfile in "${!dots[@]}"; do
-    out="${dots[$dotfile]}"
-    echo -en " ${COLOR_CYAN}Uninstall${NO_COLOR} ${dotfile}"
+	for dotfile in "${!dots[@]}"; do
+		out="${dots[$dotfile]}"
+		echo -en " ${COLOR_CYAN}Uninstall${NO_COLOR} ${dotfile}"
 
-    if _exists "$out"; then
+		if _exists "$out"; then
 			unlink "$out"
 			echo -e " [ ${COLOR_GREEN}OK${NO_COLOR} ]"
 		else
 			echo -e " [ ${COLOR_RED}IGNORED${NO_COLOR} ]"
 		fi
-  done
+	done
 }
 
 case "$1" in
-  link)
-    shift && _link "$@"
-    ;;
-  unlink)
-    shift && _unlink "$@"
-    ;;
-  *) cat <<EOF
+link)
+	shift && _link "$@"
+	;;
+unlink)
+	shift && _unlink "$@"
+	;;
+*)
+	cat <<EOF
 dot - 0.1.0
 
 USAGE:
  link   # Link user configuration
  unlink # Unlink user configuration
 EOF
-;;
+	;;
 esac
